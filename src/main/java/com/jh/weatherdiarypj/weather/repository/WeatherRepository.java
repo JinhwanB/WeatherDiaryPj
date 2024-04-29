0988276726f29161e892b10c9af5440b90e4e6eb
jinhwan Bea <paewhal20@gmail.com>
2024-04-30 07:12:20 +0900

    문자열 날짜를 통해 날씨 정보 가져오는 메소드 추가

--- src/main/java/com/jh/weatherdiarypj/weather/repository/WeatherRepository.java
+++ src/main/java/com/jh/weatherdiarypj/weather/repository/WeatherRepository.java
@@ -4,6 +4,9 @@ import com.jh.weatherdiarypj.weather.domain.Weather;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface WeatherRepository extends JpaRepository<Weather, Long> {
+    Optional<Weather> findByRegDate(String date);
 }
