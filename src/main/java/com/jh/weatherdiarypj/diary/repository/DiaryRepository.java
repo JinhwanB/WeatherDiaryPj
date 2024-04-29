6fe718af11424d4d750f6ad57ae2c6b55572dcfe
jinhwan Bea <paewhal20@gmail.com>
2024-04-30 06:11:50 +0900

    필드 변경으로 인한 수정

--- src/main/java/com/jh/weatherdiarypj/diary/repository/DiaryRepository.java
+++ src/main/java/com/jh/weatherdiarypj/diary/repository/DiaryRepository.java
@@ -4,10 +4,9 @@ import com.jh.weatherdiarypj.diary.domain.Diary;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDateTime;
 import java.util.Optional;
 
 @Repository
 public interface DiaryRepository extends JpaRepository<Diary, Long> {
-    Optional<Diary> findByRegDate(LocalDateTime date);
+    Optional<Diary> findByRegDate(String date);
 }
