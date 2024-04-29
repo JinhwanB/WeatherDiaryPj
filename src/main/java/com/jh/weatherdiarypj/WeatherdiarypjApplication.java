95851ff091df264c1e40498f45e56d45c4f5587a
jinhwan Bea <paewhal20@gmail.com>
2024-04-30 04:22:42 +0900

    Update WeatherdiarypjApplication.java

--- src/main/java/com/jh/weatherdiarypj/WeatherdiarypjApplication.java
+++ src/main/java/com/jh/weatherdiarypj/WeatherdiarypjApplication.java
@@ -2,12 +2,14 @@ package com.jh.weatherdiarypj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class WeatherdiarypjApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WeatherdiarypjApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherdiarypjApplication.class, args);
+    }
 
 }
