295ae9d7e2d9878f502246ccc5305074ff96687d
jinhwan Bea <paewhal20@gmail.com>
2024-04-30 08:16:17 +0900

    시작날짜부터 끝날짜까지의 일기 리스트 조회 서비스 테스트 추가

--- src/test/java/com/jh/weatherdiarypj/diary/service/DiaryServiceTest.java
+++ src/test/java/com/jh/weatherdiarypj/diary/service/DiaryServiceTest.java
@@ -3,6 +3,7 @@ package com.jh.weatherdiarypj.diary.service;
 import com.jh.weatherdiarypj.diary.domain.Diary;
 import com.jh.weatherdiarypj.diary.exception.DiaryException;
 import com.jh.weatherdiarypj.diary.exception.DiaryExceptionCode;
+import com.jh.weatherdiarypj.diary.repository.DiaryRepository;
 import com.jh.weatherdiarypj.weather.domain.Weather;
 import com.jh.weatherdiarypj.weather.repository.WeatherRepository;
 import org.junit.jupiter.api.DisplayName;
@@ -12,6 +13,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -24,6 +26,9 @@ class DiaryServiceTest {
 
     @Autowired
     private WeatherRepository weatherRepository;
+
+    @Autowired
+    private DiaryRepository diaryRepository;
 
     @Test
     @DisplayName("일기 작성 성공 테스트 - 날씨 데이터가 없을 경우")
@@ -87,5 +92,32 @@ class DiaryServiceTest {
         } catch (DiaryException e) {
             assertThat(e.getMessage()).isEqualTo(DiaryExceptionCode.NOT_FOUNT_DIARY.getMessage());
         }
+    }
+
+    @Test
+    @DisplayName("시작날짜와 끝날짜로 일기 리스트 조회 테스트")
+        // 일기 엔티티의 @CreatedDate 주석 후 진행
+    void readDiaryStartEnd() {
+        Diary diary1 = Diary.builder()
+                .date("1234-12-12")
+                .ico("ico")
+                .weather("weather")
+                .temp(123.12)
+                .content("test")
+                .regDate(LocalDateTime.now())
+                .build();
+        Diary diary2 = diary1.toBuilder()
+                .regDate(diary1.getRegDate().plusDays(1))
+                .build();
+        Diary diary3 = diary2.toBuilder()
+                .regDate(diary2.getRegDate().plusDays(1))
+                .build();
+        diaryRepository.save(diary1);
+        diaryRepository.save(diary2);
+        diaryRepository.save(diary3);
+
+        List<Diary> diaries = diaryService.getDiaries("2024-04-30", "2024-05-01");
+
+        assertThat(diaries).hasSize(2);
     }
 }
