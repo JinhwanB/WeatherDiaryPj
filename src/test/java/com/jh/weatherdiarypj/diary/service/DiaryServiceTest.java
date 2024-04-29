de3a84e77edd699bbc82935327350ae4002a5279
jinhwan Bea <paewhal20@gmail.com>
2024-04-30 07:35:57 +0900

    일기 작성 서비스 테스트 추가

---
+++ src/test/java/com/jh/weatherdiarypj/diary/service/DiaryServiceTest.java
@@ -0,0 +1,48 @@
+package com.jh.weatherdiarypj.diary.service;
+
+import com.jh.weatherdiarypj.diary.domain.Diary;
+import com.jh.weatherdiarypj.weather.domain.Weather;
+import com.jh.weatherdiarypj.weather.repository.WeatherRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
+class DiaryServiceTest {
+    @Autowired
+    private DiaryService diaryService;
+
+    @Autowired
+    private WeatherRepository weatherRepository;
+
+    @Test
+    @DisplayName("일기 작성 성공 테스트 - 날씨 데이터가 없을 경우")
+    void createDiaryWithoutWeather() throws IOException {
+        Diary diary = diaryService.createDiary("2024-04-30", "test");
+
+        assertThat(diary.getContent()).isEqualTo("test");
+    }
+
+    @Test
+    @DisplayName("일기 작성 성공 테스트 - 날씨 데이터가 있는 경우")
+    void createDiaryWithWeather() throws IOException {
+        Weather weather = Weather.builder()
+                .ico("ico")
+                .regDate("2024-04-30")
+                .temp(123.12)
+                .weather("weather")
+                .build();
+        weatherRepository.save(weather);
+
+        Diary diary = diaryService.createDiary("2024-04-30", "test");
+
+        assertThat(diary.getContent()).isEqualTo("test");
+    }
+}
