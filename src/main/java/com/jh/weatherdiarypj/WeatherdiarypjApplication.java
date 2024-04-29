1ea54089f973017286b8eb71901e50504ae27508
jinhwan Bea <paewhal20@gmail.com>
2024-04-29 15:21:24 +0900

    first init

---
+++ src/main/java/com/jh/weatherdiarypj/WeatherdiarypjApplication.java
@@ -0,0 +1,13 @@
+package com.jh.weatherdiarypj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherdiarypjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherdiarypjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jh/weatherdiarypj/WeatherdiarypjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jh.weatherdiarypj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherdiarypjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
