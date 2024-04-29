2d27f13e6a64b86a2a42e9d86ac5bff3e46e2091
jinhwan Bea <paewhal20@gmail.com>
2024-04-30 06:24:37 +0900

    삭제되지 않은 일기만 조회하도록 변경

--- src/main/java/com/jh/weatherdiarypj/diary/repository/DiaryRepository.java
+++ src/main/java/com/jh/weatherdiarypj/diary/repository/DiaryRepository.java
@@ -4,9 +4,10 @@ import com.jh.weatherdiarypj.diary.domain.Diary;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
 public interface DiaryRepository extends JpaRepository<Diary, Long> {
-    List<Diary> findAllByRegDate(String date);
+    List<Diary> findAllByRegDateAndDelDate(String date, LocalDateTime delDate);
 }
