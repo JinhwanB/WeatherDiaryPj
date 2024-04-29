05933157929147072a83a4526b3048e570c62d71
jinhwan Bea <paewhal20@gmail.com>
2024-04-30 05:02:44 +0900

    날씨 exception 생성

---
+++ src/main/java/com/jh/weatherdiarypj/weather/exception/WeatherException.java
@@ -0,0 +1,11 @@
+package com.jh.weatherdiarypj.weather.exception;
+
+public class WeatherException extends RuntimeException {
+    public WeatherException() {
+        super();
+    }
+
+    public WeatherException(String message) {
+        super(message);
+    }
+}
