0de104d6cda22cd1023f8cd5e73f6e0fd2b266e2
jinhwan Bea <paewhal20@gmail.com>
2024-04-30 15:24:04 +0900

    Update DiaryController.java

--- src/main/java/com/jh/weatherdiarypj/diary/controller/DiaryController.java
+++ src/main/java/com/jh/weatherdiarypj/diary/controller/DiaryController.java
@@ -9,6 +9,7 @@ import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.Parameters;
 import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
@@ -39,10 +40,11 @@ public class DiaryController {
             @Parameter(name = "text", description = "작성할 일기의 내용")
     })
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "성공", content = @Content(schema = @Schema(implementation = GlobalApiResponse.class))),
-            @ApiResponse(responseCode = "400", description = "파라미터 유효성 검증 실패", content = @Content(array = @ArraySchema(schema = @Schema(implementation = GlobalApiResponse.class)))),
-            @ApiResponse(responseCode = "500", description = "예상치 못한 오류", content = @Content(schema = @Schema(implementation = GlobalApiResponse.class)))
+            @ApiResponse(responseCode = "200", description = "성공", content = @Content(schema = @Schema(implementation = GlobalApiResponse.class), examples = @ExampleObject(value = "{\n  \"status\": 200,\n  \"message\": \"성공\",\n \"result\": [\n {\n \"content\": \"good\",\n \"weather\": \"Clouds\",\n \"ico\": \"04d\",\n \"temp\": 294.56,\n \"writeDate\": \"1234-12-12\"\n}\n]}"))),
+            @ApiResponse(responseCode = "400", description = "파라미터 유효성 검증 실패", content = @Content(array = @ArraySchema(schema = @Schema(implementation = GlobalApiResponse.class)), examples = @ExampleObject(value = "{\n  \"status\": 400,\n  \"message\": \"파라미터 유효성 검증 실패\",\n \"result\": []}"))),
+            @ApiResponse(responseCode = "500", description = "예상치 못한 오류", content = @Content(schema = @Schema(implementation = GlobalApiResponse.class), examples = @ExampleObject(value = "{\n  \"status\": 500,\n  \"message\": \"예상치 못한 오류\",\n \"result\": []}")))
     })
+
     @PostMapping("/diary")
     public ResponseEntity<GlobalApiResponse> create(
             @RequestParam @Pattern(regexp = "^\\d{4}-\\d{2}-\\d{2}$", message = "날짜는 yyyy-mm-dd로 작성해야 합니다.") String date,
@@ -62,10 +64,10 @@ public class DiaryController {
             @Parameter(name = "text", description = "수정할 일기의 내용")
     })
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "성공", content = @Content(schema = @Schema(implementation = GlobalApiResponse.class))),
-            @ApiResponse(responseCode = "400", description = "파라미터 유효성 검증 실패", content = @Content(array = @ArraySchema(schema = @Schema(implementation = GlobalApiResponse.class)))),
-            @ApiResponse(responseCode = "404", description = "수정하려는 일기가 존재하지 않는 경우", content = @Content(schema = @Schema(implementation = GlobalApiResponse.class))),
-            @ApiResponse(responseCode = "500", description = "예상치 못한 오류", content = @Content(schema = @Schema(implementation = GlobalApiResponse.class)))
+            @ApiResponse(responseCode = "200", description = "성공", content = @Content(schema = @Schema(implementation = GlobalApiResponse.class), examples = @ExampleObject(value = "{\n  \"status\": 200,\n  \"message\": \"성공\",\n \"result\": [\n {\n \"content\": \"good\",\n \"weather\": \"Clouds\",\n \"ico\": \"04d\",\n \"temp\": 294.56,\n \"writeDate\": \"1234-12-12\"\n}\n]}"))),
+            @ApiResponse(responseCode = "404", description = "수정하려는 일기가 존재하지 않는 경우", content = @Content(schema = @Schema(implementation = GlobalApiResponse.class), examples = @ExampleObject(value = "{\n  \"status\": 404,\n  \"message\": \"일기를 찾을 수 없습니다.\",\n \"result\": []}"))),
+            @ApiResponse(responseCode = "400", description = "파라미터 유효성 검증 실패", content = @Content(array = @ArraySchema(schema = @Schema(implementation = GlobalApiResponse.class)), examples = @ExampleObject(value = "{\n  \"status\": 400,\n  \"message\": \"파라미터 유효성 검증 실패\",\n \"result\": []}"))),
+            @ApiResponse(responseCode = "500", description = "예상치 못한 오류", content = @Content(schema = @Schema(implementation = GlobalApiResponse.class), examples = @ExampleObject(value = "{\n  \"status\": 500,\n  \"message\": \"예상치 못한 오류\",\n \"result\": []}")))
     })
     @PutMapping("/diary")
     public ResponseEntity<GlobalApiResponse> update(
@@ -83,10 +85,10 @@ public class DiaryController {
     @Operation(summary = "일기 삭제")
     @Parameter(name = "date", description = "삭제할 일기의 날짜")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "성공", content = @Content(schema = @Schema(implementation = GlobalApiResponse.class))),
-            @ApiResponse(responseCode = "400", description = "파라미터 유효성 검증 실패", content = @Content(array = @ArraySchema(schema = @Schema(implementation = GlobalApiResponse.class)))),
-            @ApiResponse(responseCode = "404", description = "삭제하려는 일기가 존재하지 않는 경우", content = @Content(schema = @Schema(implementation = GlobalApiResponse.class))),
-            @ApiResponse(responseCode = "500", description = "예상치 못한 오류", content = @Content(schema = @Schema(implementation = GlobalApiResponse.class)))
+            @ApiResponse(responseCode = "200", description = "성공", content = @Content(schema = @Schema(implementation = GlobalApiResponse.class), examples = @ExampleObject(value = "{\n  \"status\": 200,\n  \"message\": \"성공\",\n \"result\": []}"))),
+            @ApiResponse(responseCode = "404", description = "삭제하려는 일기가 존재하지 않는 경우", content = @Content(schema = @Schema(implementation = GlobalApiResponse.class), examples = @ExampleObject(value = "{\n  \"status\": 404,\n  \"message\": \"일기를 찾을 수 없습니다.\",\n \"result\": []}"))),
+            @ApiResponse(responseCode = "400", description = "파라미터 유효성 검증 실패", content = @Content(array = @ArraySchema(schema = @Schema(implementation = GlobalApiResponse.class)), examples = @ExampleObject(value = "{\n  \"status\": 400,\n  \"message\": \"파라미터 유효성 검증 실패\",\n \"result\": []}"))),
+            @ApiResponse(responseCode = "500", description = "예상치 못한 오류", content = @Content(schema = @Schema(implementation = GlobalApiResponse.class), examples = @ExampleObject(value = "{\n  \"status\": 500,\n  \"message\": \"예상치 못한 오류\",\n \"result\": []}")))
     })
     @DeleteMapping("/diary")
     public ResponseEntity<GlobalApiResponse> delete(
@@ -105,9 +107,9 @@ public class DiaryController {
     @Operation(summary = "일기 조회")
     @Parameter(name = "date", description = "삭제할 일기의 날짜")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "성공", content = @Content(schema = @Schema(implementation = GlobalApiResponse.class))),
-            @ApiResponse(responseCode = "400", description = "파라미터 유효성 검증 실패", content = @Content(array = @ArraySchema(schema = @Schema(implementation = GlobalApiResponse.class)))),
-            @ApiResponse(responseCode = "500", description = "예상치 못한 오류", content = @Content(schema = @Schema(implementation = GlobalApiResponse.class)))
+            @ApiResponse(responseCode = "200", description = "성공", content = @Content(schema = @Schema(implementation = GlobalApiResponse.class), examples = @ExampleObject(value = "{\n  \"status\": 200,\n  \"message\": \"성공\",\n \"result\": [\n {\n \"content\": \"good\",\n \"weather\": \"Clouds\",\n \"ico\": \"04d\",\n \"temp\": 294.56,\n \"writeDate\": \"1234-12-12\"\n}\n]}"))),
+            @ApiResponse(responseCode = "400", description = "파라미터 유효성 검증 실패", content = @Content(array = @ArraySchema(schema = @Schema(implementation = GlobalApiResponse.class)), examples = @ExampleObject(value = "{\n  \"status\": 400,\n  \"message\": \"파라미터 유효성 검증 실패\",\n \"result\": []}"))),
+            @ApiResponse(responseCode = "500", description = "예상치 못한 오류", content = @Content(schema = @Schema(implementation = GlobalApiResponse.class), examples = @ExampleObject(value = "{\n  \"status\": 500,\n  \"message\": \"예상치 못한 오류\",\n \"result\": []}")))
     })
     @GetMapping("/diary")
     public ResponseEntity<GlobalApiResponse> get(
@@ -126,9 +128,9 @@ public class DiaryController {
             @Parameter(name = "endDate", description = "조회할 일기 범위의 끝날짜")
     })
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "성공", content = @Content(schema = @Schema(implementation = GlobalApiResponse.class))),
-            @ApiResponse(responseCode = "400", description = "파라미터 유효성 검증 실패", content = @Content(array = @ArraySchema(schema = @Schema(implementation = GlobalApiResponse.class)))),
-            @ApiResponse(responseCode = "500", description = "예상치 못한 오류", content = @Content(schema = @Schema(implementation = GlobalApiResponse.class)))
+            @ApiResponse(responseCode = "200", description = "성공", content = @Content(schema = @Schema(implementation = GlobalApiResponse.class), examples = @ExampleObject(value = "{\n  \"status\": 200,\n  \"message\": \"성공\",\n \"result\": [\n {\n \"content\": \"good\",\n \"weather\": \"Clouds\",\n \"ico\": \"04d\",\n \"temp\": 294.56,\n \"writeDate\": \"1234-12-12\"\n}\n]}"))),
+            @ApiResponse(responseCode = "400", description = "파라미터 유효성 검증 실패", content = @Content(array = @ArraySchema(schema = @Schema(implementation = GlobalApiResponse.class)), examples = @ExampleObject(value = "{\n  \"status\": 400,\n  \"message\": \"파라미터 유효성 검증 실패\",\n \"result\": []}"))),
+            @ApiResponse(responseCode = "500", description = "예상치 못한 오류", content = @Content(schema = @Schema(implementation = GlobalApiResponse.class), examples = @ExampleObject(value = "{\n  \"status\": 500,\n  \"message\": \"예상치 못한 오류\",\n \"result\": []}")))
     })
     @GetMapping
     public ResponseEntity<GlobalApiResponse> getFromStartEnd(
