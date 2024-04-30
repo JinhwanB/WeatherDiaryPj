a99ecc6ea1308db8e53481f2828716e90b9f35b7
jinhwan Bea <paewhal20@gmail.com>
2024-04-30 14:25:52 +0900

    Update ApiResponse.java

---
+++ src/main/java/com/jh/weatherdiarypj/config/ApiResponse.java
@@ -0,0 +1,23 @@
+package com.jh.weatherdiarypj.config;
+
+import com.jh.weatherdiarypj.diary.dto.DiaryResponseDto;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.*;
+
+@Getter
+@Setter // 테스트용
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder(toBuilder = true)
+@ToString
+@Schema(description = "전체 api 응답 response")
+public class ApiResponse {
+    @Schema(description = "http 상태 코드", examples = {"200", "400", "404", "500"})
+    private int status;
+
+    @Schema(description = "예외에 대한 설명", examples = {"성공", "유효성 검사 실패", "일기를 찾을 수 없습니다.", "서버에 문의하세요."})
+    private String message;
+
+    @Schema(description = "성공 시 응답 결과", implementation = DiaryResponseDto.class)
+    private DiaryResponseDto result;
+}
