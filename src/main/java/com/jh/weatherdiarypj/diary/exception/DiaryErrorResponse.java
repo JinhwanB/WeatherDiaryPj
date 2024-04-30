c19e9c8d72da4dff6c827291e629249ef985ee45
jinhwan Bea <paewhal20@gmail.com>
2024-04-30 12:35:15 +0900

    Update DiaryErrorResponse.java

--- src/main/java/com/jh/weatherdiarypj/diary/exception/DiaryErrorResponse.java
+++ src/main/java/com/jh/weatherdiarypj/diary/exception/DiaryErrorResponse.java
@@ -1,12 +1,14 @@
 package com.jh.weatherdiarypj.diary.exception;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@Builder
 public class DiaryErrorResponse {
     private int status;
     private String message;
