97d29e878c2b7c41198d035ed8f5f1119488dfdb
jinhwan Bea <paewhal20@gmail.com>
2024-04-30 13:53:14 +0900

    Update DiaryErrorResponse.java

--- src/main/java/com/jh/weatherdiarypj/diary/exception/DiaryErrorResponse.java
+++ src/main/java/com/jh/weatherdiarypj/diary/exception/DiaryErrorResponse.java
@@ -1,5 +1,6 @@
 package com.jh.weatherdiarypj.diary.exception;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -9,7 +10,11 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @Builder
+@Schema(description = "예외 응답 response")
 public class DiaryErrorResponse {
+    @Schema(description = "http 상태 코드", example = "404")
     private int status;
+
+    @Schema(description = "예외에 대한 설명", example = "예외가 발생했습니다.")
     private String message;
 }
